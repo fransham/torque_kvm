b21af57df5642682326b38613cb038c97e2e90aa
Kyle Fransham <fransham@uvic.ca>
2011-05-05 14:09:03 -0400

    bug fix

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@
 #
 #########################################################
 
+SCRIPTDIR=$(cd `dirname $0` && echo `pwd`)
+
 #set this parameter for your torque install on the worker node:
 TORQUE_HOME=/var/spool/torque
 
@@ -14,9 +16,9 @@ mkdir -p $TORQUE_HOME/net
 
 chmod 1777 $TORQUE_HOME/virt
 
-cp -v bin/prologue $TORQUE_HOME/virt/prologue
-cp -v bin/epilogue $TORQUE_HOME/virt/epilogue
-cp -v net/* $TORQUE_HOME/net/
+cp -v $SCRIPTDIR/bin/prologue $TORQUE_HOME/virt/prologue
+cp -v $SCRIPTDIR/bin/epilogue $TORQUE_HOME/virt/epilogue
+cp -v $SCRIPTDIR/net/* $TORQUE_HOME/net/
 
 $TORQUE_HOME/net/initialize
 
