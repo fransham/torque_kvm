55a03f830b24536ddb7ccf10bfefe02291e4567c
Kyle Fransham <fransham@uvic.ca>
2011-05-10 14:05:14 -0400

    bug fix

--- profile.d/torque-interactive.sh
+++ profile.d/torque-interactive.sh
@@ -9,25 +9,22 @@
 #############################################################
 
 #if we're not in an interactive session, then exit
-if [ ! "$PBS_ENVIRONMENT" = "PBS_INTERACTIVE" ]; then
-  exit 0	
+if [ "$PBS_ENVIRONMENT" = "PBS_INTERACTIVE" ]; then
+
+  TARGET_HOST=`cat /var/spool/torque/virt/${PBS_JOBID}`
+  if [ ! -f $TARGET_HOST ]; then
+    echo "Cannot find job...  exiting"
+    exit 1
+  fi
+
+  echo "Connecting to virtual machine... please wait"
+  
+  # stall to make sure the VM is online:
+  ssh -o ConnectionAttempts=300 ${TARGET_HOST} "/bin/true"
+
+  # connect to the VM:
+  ssh -t ${TARGET_HOST} "source /tmp/${PBS_JOBID};/bin/bash -i"
+
+  # exit when we're done.
+  exit 0
 fi
-
-
-# trap keyboard interrupt (control-c)
-trap control_c SIGINT
-
-# run the user's job:
-TORQUE_DIR=/var/spool/torque/mom_priv/jobs
-USERJOB=${TORQUE_DIR}/${PBS_JOBID}.SC
-
-if [ ! -f $USERJOB ]; then
-  echo "Cannot find job...  exiting"
-  exit 1
-fi
-
-echo "Connecting to virtual machine... please wait"
-
-sh $USERJOB
-
-
