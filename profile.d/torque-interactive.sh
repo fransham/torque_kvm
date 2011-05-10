dcdd601749da43aea00a9cc35a8099dcdd5e69b4
Kyle Fransham <fransham@uvic.ca>
2011-05-10 13:56:58 -0400

    bug fix

--- profile.d/torque-interactive.sh
+++ profile.d/torque-interactive.sh
@@ -10,21 +10,10 @@
 
 #if we're not in an interactive session, then exit
 if [ ! "$PBS_ENVIRONMENT" = "PBS_INTERACTIVE" ]; then
-	exit 0	
+  exit 0	
 fi
 
-#finish the interactive session.
-do_logout(){
-  kill -HUP `pgrep -s 0 -o`
-}
 
-control_c()
-# run if user hits control-c
-{
-  echo -en "\n*** Exiting Interactive Session ***\n"
-  do_logout
-}
- 
 # trap keyboard interrupt (control-c)
 trap control_c SIGINT
 
@@ -34,12 +23,11 @@ USERJOB=${TORQUE_DIR}/${PBS_JOBID}.SC
 
 if [ ! -f $USERJOB ]; then
   echo "Cannot find job...  exiting"
+  exit 1
 fi
 
 echo "Connecting to virtual machine... please wait"
 
 sh $USERJOB
 
-#all done.  kill the interactive session:
-do_logout
 
