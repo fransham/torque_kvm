faaba2ac472d2ef1f9466d443bf2220c4f6525fa
Kyle Fransham <fransham@uvic.ca>
2011-05-11 09:56:09 -0400

    bug fix

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ chmod 1777 $TORQUE_HOME/virt
 
 cp -v $SCRIPTDIR/bin/prologue $TORQUE_HOME/mom_priv/prologue
 cp -v $SCRIPTDIR/bin/epilogue $TORQUE_HOME/mom_priv/epilogue
-cp -i -v $SCRIPTDIR/net/* $TORQUE_HOME/net/
+cp -i $SCRIPTDIR/net/initialize $TORQUE_HOME/net/
+cp -i -v $SCRIPTDIR/net/network.conf $TORQUE_HOME/net/
 cp -v $SCRIPTDIR/profile.d/torque-interactive.* /etc/profile.d/
 
 
